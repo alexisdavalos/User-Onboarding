336a245fd58091c90f78518950afc7c3303b7317
Alexis <alexisdavalos.tech@gmail.com>
2019-12-19 23:49:09 -0500

    Basic Form Styled

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,9 +1,50 @@
 import React from 'react';
-
-const Form = (props) =>{
+import {withFormik} from 'formik';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import styled from 'styled-components'
+const Form_S = styled(Form)`
+width:50%;
+margin: 0 auto;
+`
+const SignupForm = ({values,handleChange}) =>{
     return (
-        <div>This is our form</div>
+        
+            <Form_S>
+                <Label>Name</Label>
+                <Input 
+                type='text'
+                value={values.name}
+                onChange={handleChange}
+                name='name'
+                />
+                <Label>Email</Label>
+                <Input 
+                type='email'
+                name='email'
+                onChange={handleChange}
+                />
+                <Label>Password</Label>
+                <Input 
+                type='password'
+                name='password'
+                onChange={handleChange}
+                />
+                <Button>Submit</Button>
+            </Form_S>
+
+
+       
     )
 }
-
-export default Form;
+const FormikForm = withFormik({
+    mapPropsToValues(props){
+        return {
+            name: props.name,
+            email: props.email,
+            password: props.password,
+            terms: props.terms
+        }
+    }
+})(SignupForm);
+//replaced Form with FormikForm
+export default FormikForm;
